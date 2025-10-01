b58a327c040d42cfe19ef31d77b07a382dab096c
yuri-anjos <yuri.dos.4njos@gmail.com>
2025-10-01 14:12:04 -0300

    fix security config

--- projeto-ms/eureka-server/src/main/java/com/yuri/eurekaserver/config/SecurityConfig.java
+++ projeto-ms/eureka-server/src/main/java/com/yuri/eurekaserver/config/SecurityConfig.java
@@ -6,6 +6,12 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -15,10 +21,35 @@ public class SecurityConfig {
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http.authorizeHttpRequests(config -> config
-				.anyRequest().authenticated());
+				.requestMatchers("/public").permitAll()
+				.requestMatchers("/health").permitAll()
+				.anyRequest().authenticated()
+		);
 		http.csrf(AbstractHttpConfigurer::disable);
 		http.httpBasic(Customizer.withDefaults());
 		http.formLogin(Customizer.withDefaults());
 		return http.build();
+	}
+
+	@Bean
+	public UserDetailsService userDetailsService() {
+		UserDetails user = User.builder()
+				.username("user")
+				.password(passwordEncoder().encode("123"))
+				.roles("USER")
+				.build();
+
+		UserDetails admin = User.builder()
+				.username("admin")
+				.password(passwordEncoder().encode("admin"))
+				.roles("ADMIN")
+				.build();
+
+		return new InMemoryUserDetailsManager(user, admin);
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 }
